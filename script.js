fda52a138fabae25dcca6453bbd60430eb948fcb
ghulam-seo <131991438+gnabi2002@users.noreply.github.com>
2025-12-04 16:09:31 +0000

    udate on navbar

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  // Toggle menu + animate hamburger
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");   // slide down menu
+    hamburger.classList.toggle("active");  // animate into X
+  });
+});
